33e0d037b37d38cb031c572af8493a574757992b
Caleb James DeLisle <cjd@cjdns.fr>
2019-05-03 09:29:37 +0200

    Attempt to get travisci building for mac

---
+++ .travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+die() { exit 1; }
+eval "${MATRIX_EVAL}";
+if test "x$LINT" = "x1"; then
+    npm install || die
+    npm run lint || die
+    npm run flow || die
+else
+    if test "`uname`" = "Darwin"; then
+        brew install libsodium pkg-config || die
+        ./autogen.sh || die
+        PKG_CONFIG_PATH=`echo /usr/local/Cellar/libsodium/*/lib/pkgconfig` ./configure || die
+        make || die
+    else
+        ./autogen.sh || die
+        ./configure CC=$CC CXX=$CXX || die
+        make; || die
+    fi
+fi
