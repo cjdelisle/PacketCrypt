8ca5c8c07fc60ef50947c2f877b6b414b5879c92
Caleb James DeLisle <cjd@cjdns.fr>
2019-05-03 09:44:28 +0200

    Travisci/osx: Try with autoconf-archive

--- .travis.sh
+++ .travis.sh
@@ -7,7 +7,7 @@ if test "x$LINT" = "x1"; then
     npm run flow || die
 else
     if test "`uname`" = "Darwin"; then
-        brew install libsodium pkg-config || die
+        brew install libsodium pkg-config autoconf-archive || die
         ./autogen.sh || die
         PKG_CONFIG_PATH=`echo /usr/local/Cellar/libsodium/*/lib/pkgconfig` ./configure || die
         make || die
