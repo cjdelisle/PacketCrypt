65f9f7b81df310ed20cd839b9114c634eaa23289
Caleb James DeLisle <cjd@cjdns.fr>
2021-01-16 21:29:36 +0100

    Fix coinbaseCommitDecode 2

--- js/Protocol.js
+++ js/Protocol.js
@@ -370,9 +370,9 @@ module.exports.shareFileEncode = (share /*:Protocol_ShareFile_t*/) /*:Buffer*/ =
 };
 
 module.exports.coinbaseCommitDecode = (buf /*:Buffer*/) /*:Protocol_CoinbaseCommit_t*/ => {
-    // Skip the push bytes and magic
+    // Skip the magic
     let i = 8;
-    const annMinWork = buf.readUInt32LE(0); i += 4;
+    const annMinWork = buf.readUInt32LE(i); i += 4;
     const merkleRoot = buf.slice(i, i + 32); i += 32;
     const annCountLow = BigInt(buf.readUInt32LE(i)); i += 4;
     const annCountHigh = (BigInt(buf.readUInt32LE(i)) * BigInt(2) ** BigInt(32)); i += 4;
