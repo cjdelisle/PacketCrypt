5ed82937269b2dfa31f750cdf9999d6c97b45196
Caleb James DeLisle <cjd@cjdns.fr>
2021-03-25 01:01:37 +0100

    Add autogen which warns about outdated instructions

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo
+echo 'STOP'
+echo
+echo 'You are probably following outdated instructions'
+echo
+echo 'For PacketCrypt mining instructions go to https://docs.pkt.cash/en/latest/mining/'
+echo
